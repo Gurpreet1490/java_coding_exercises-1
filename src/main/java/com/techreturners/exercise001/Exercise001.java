b4faf79bfd32e87954dc25fb7de9d9db659aa748
Gurpreet1490 <g.preet08092014@gmail.com>
2021-12-07 16:53:58 +0000

    First Ex01

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -4,27 +4,33 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        return firstName.charAt(0)+ "." + lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        double addingVat = originalPrice + (originalPrice * vatRate) / 100;
+        return Math.round((addingVat * 100)/100);
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        if (sentence == null){
+            return null;
+        }
+        StringBuilder output = new StringBuilder(sentence).reverse();
+        return output.toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        int countLinux = 0;
+        for (User myOs : users){
+            if (myOs.getType().equals("Linux")){
+                countLinux += 1;
+            }
+        }
+        return countLinux;
     }
 }
