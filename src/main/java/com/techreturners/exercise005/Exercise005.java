348f64c0c5ea2f695232bb96e234a91ad78b7047
Gurpreet1490 <g.preet08092014@gmail.com>
2021-12-10 11:40:56 +0000

    Ex05 pangram

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,32 @@
 package com.techreturners.exercise005;
 
+import java.util.Arrays;
+
 public class Exercise005 {
 
+    private static final int ALPHABET_COUNT = 26;
+
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        if (input == null) {
+            return false;
+        }
+        Boolean[] alphabetMarker = new Boolean[ALPHABET_COUNT];
+        Arrays.fill(alphabetMarker, false);
+        int alphabetIndex = 0;
+        input = input.toUpperCase();
+        for (int i = 0; i < input.length(); i++) {
+            if ('A' <= input.charAt(i) && input.charAt(i) <= 'Z') {
+                alphabetIndex = input.charAt(i) - 'A';
+                alphabetMarker[alphabetIndex] = true;
+            }
+        }
+        for (boolean index : alphabetMarker) {
+            if (!index) {
+                return false;
+            }
+        }
+        return true;
+
     }
 
 }
